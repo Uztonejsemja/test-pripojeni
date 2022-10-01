ca02eb5f6aae6f5deb03d058a593c1a5af606f65
Uztonejsemja <98396890+Uztonejsemja@users.noreply.github.com>
2022-10-01 21:45:27 +0200

    Update index.jsx

--- src/components/Offline/index.jsx
+++ src/components/Offline/index.jsx
@@ -3,6 +3,31 @@ import React, { useEffect, useState } from 'react';
 const Offline = () => {
   const [isOnline, setIsOnline] = useState(navigator.onLine);
 
+  useEffect(
+    () => {
+      console.log('mounted');
+      window.addEventListener('offline', handleOffline);
+      window.addEventListener('online', handleOnline);
+
+    return () => {
+      console.log('unmounted');
+      window.removeEventListener('offline', handleOffline);
+      window.removeEventListener('online', handleOnline);
+    }
+  },
+    []
+  );
+
+  const handleOffline = () => {
+    setIsOnline(false);
+    console.log('offline');
+  };
+
+  const handleOnline = () => {
+    setIsOnline(true)
+  };
+
+
   if (isOnline) {
     return (
       <p>Všechno v pořádku</p>
